5a6430d8775aa6158b142451c8f78e22cb1c6fd4
meham4965-dev <meham4965@gmail.com>
2026-02-18 10:39:00 +0530

    Add files via upload

---
+++ for loop prog 3 (2).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, units, total = 0;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &units);
+        total += units;
+    }
+    printf("Total Units: %d", total);
+
+    return 0;
+}
