c8f8f8806e3e18fd171fc969246a39bbf27bdf92
meham4965-dev <meham4965@gmail.com>
2026-02-18 10:35:38 +0530

    Add files via upload

---
+++ for loop prog 2 (2).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, amount;
+    int total = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &amount);
+        total += amount;
+    }
+
+    if(total > 10000) {
+        printf("Limit Exceeded");
+    } else {
+        printf("Approved");
+    }
+
+    return 0;
+}
